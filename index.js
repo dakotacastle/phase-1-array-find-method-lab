a14d9e44bc82e72122f13642f3e5b8a39b5276fe
Dakota Castle <barrettcastle@gmail.com>
2021-11-29 09:54:07 -0500

    commit tree 44610092d8a5765c867ab1f157a716a408220532

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+const superbowlWin = (array) => {
+    const obj = array.find((record) => record.result === "W")
+    if (obj !== undefined) {
+        return obj.year
+    } else {
+        return undefined
+    }
+}
